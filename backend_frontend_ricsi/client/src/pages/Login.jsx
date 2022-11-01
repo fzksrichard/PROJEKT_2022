b11d03351be5c0cca096123e46d5103d26e7181e
fzksrichard <fazekasricsi99@gmail.com>
2022-11-02 00:02:06 +0100

    Login

--- backend_frontend_ricsi/client/src/pages/Login.jsx
+++ backend_frontend_ricsi/client/src/pages/Login.jsx
@@ -1,6 +1,11 @@
 import { MobileFriendly } from "@material-ui/icons";
 import styled from "styled-components";
 import {mobile} from "../responsive";
+import { login } from "../redux/apiCalls";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+
+
 
 const Container = styled.div`
   width: 100vw;
@@ -46,6 +51,10 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled{
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
@@ -55,15 +64,29 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
+const Error=styled.span`
+  color: red;
+`
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
+          <Input placeholder="username" onChange={(e)=>setUsername(e.target.value)} />
+          <Input placeholder="password" type="password" onChange={(e)=>setPassword(e.target.value)} />
+          <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
+          {error && <Error>Something went wrong</Error>} 
           <Link>DON'T REMEMBER PASSWORD?</Link>
           <Link>CREATE NEW ACCOUNT</Link>
         </Form>
@@ -73,3 +96,4 @@ const Login = () => {
 };
 
 export default Login;
+ //disabled isfetching hibás (balázs)
