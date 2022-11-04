35e1d158122971803921d5dc32f20b6ef1873b4e
fzksrichard <fazekasricsi99@gmail.com>
2022-11-05 00:00:37 +0100

    Admin végleges (2/2)

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiMHUMBV9fw9z8OzKvg3LOi1cj711n7ig",
+  authDomain: "projekt2022-62245.firebaseapp.com",
+  projectId: "projekt2022-62245",
+  storageBucket: "projekt2022-62245.appspot.com",
+  messagingSenderId: "257328400492",
+  appId: "1:257328400492:web:4dc2e91ee4a0e67cd3f049"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
